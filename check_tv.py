e77d86875995b94798e9066ba0fcc5851dc343f1
zosibafa-ai <zosibafa@gmail.com>
2026-06-12 18:36:37 +0330

    Update check_tv.py

--- check_tv.py
+++ check_tv.py
@@ -1,15 +1,6 @@
 import requests
 import os
 
-KEYWORDS = [
-    "خانه به دوش",
-    "متهم گریخت",
-    "ترش و شیرین",
-    "بزنگاه"
-]
-
-URL = "https://en.ifilmtv.ir/Schedule/Index"
-
 TOKEN = os.environ.get("TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
@@ -17,13 +8,4 @@ def send(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
-html = requests.get(URL).text
-
-found = []
-
-for k in KEYWORDS:
-    if k in html:
-        found.append(k)
-
-if found:
-    send("📺 سریال پیدا شد:\n" + "\n".join(found))
+send("📡 TEST: ربات سالم است")
